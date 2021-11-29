32f4d9d77de9f9356bbfa85732d52f270a868aef
Peter Haas <peter@peterhaas.me>
2021-11-29 10:19:48 -0500

    calculate time value

--- 007_wonderland_balance/app.py
+++ 007_wonderland_balance/app.py
@@ -14,24 +14,22 @@ from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import json
-import os
+#import os
 from pycoingecko import CoinGeckoAPI
+from token_info import tokens
 from web3 import Web3
-
-# import infura_url from .env or use your own
-# infura_url = 'insert your url here'
-load_dotenv()
-INFURA_URL = os.getenv('INFURA_URL')
-# create Infura connection
-web3 = Web3(Web3.HTTPProvider(INFURA_URL))
 
 # Dash instance
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
 
 # Coingecko API instance
 cg = CoinGeckoAPI()
+
+# create Avax connection
+avalanche_url = 'https://api.avax.network/ext/bc/C/rpc'
+web3 = Web3(Web3.HTTPProvider(avalanche_url))
 
 # Layout components
 wallet_input = [
@@ -44,7 +42,7 @@ wallet_input = [
         children=[
             dbc.Input(
                 id='wallet_input',
-                value='0xc2306a06993ebfb4a66b189c98fd1d5f03855349',
+                value='0x104d5ebb38af1ae5eb469b86922d1f10808eb35f',
                 type='text',
                 autofocus=True
             ),
@@ -67,7 +65,8 @@ time_balance = [
         width=2
     ),
     dbc.Col(
-        children='1.5',
+        id='time_balance',
+        children='',
         width=2
     ),
     dbc.Col(
@@ -84,7 +83,8 @@ time_balance = [
         width=2
     ),
     dbc.Col(
-        children='$13,500',
+        id='time_value',
+        children='',
         width=2
     )
 ]
@@ -223,59 +223,66 @@ def check_validity(value):
     return False, False
 
 
-@app.callback(
-    Output(
-        component_id='time_price',
-        component_property='children'
-    ),
-    Output(
-        component_id='memo_price',
-        component_property='children'
-    ),
-    Input(
-        component_id='price_interval',
-        component_property='n_intervals'
-    ),
-)
-def time_price(n):
-    time_price = cg.get_price(
-        ids='wonderland',
-        vs_currencies='usd'
-    )
-    time_price_2 = '$' + str(time_price['wonderland']['usd'])
-    return time_price_2, time_price_2
-
 def get_token_balance(wal_addr):
     ''' Get token balance in a wallet address
 
     Keyword arguments:
     wal_addr - wallet address
     '''
-    ctrct_addr = '0x3845badAde8e6dFF049820680d1F14bD3903a5d0'
+    ctrct_addr = tokens['time']['address']
     checksum_address = web3.toChecksumAddress(ctrct_addr)
     wal_checksum = web3.toChecksumAddress(wal_addr)
-    abi='[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newAdmin","type":"address"}],"name":"changeExecutionAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"amount","type":"uint256"}],"name":"burnFor","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"},{"name":"amount","type":"uint256"}],"name":"approveFor","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"},{"name":"amountNeeded","type":"uint256"}],"name":"addAllowanceIfNeeded","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"burn","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"isExecutionOperator","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"isSuperOperator","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"executionOperator","type":"address"},{"name":"enabled","type":"bool"}],"name":"setExecutionOperator","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getAdmin","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"amount","type":"uint256"},{"name":"gasLimit","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndExecuteWithSpecificGas","outputs":[{"name":"success","type":"bool"},{"name":"returnData","type":"bytes"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"superOperator","type":"address"},{"name":"enabled","type":"bool"}],"name":"setSuperOperator","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getExecutionAdmin","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"amount","type":"uint256"},{"name":"data","type":"bytes"}],"name":"paidCall","outputs":[{"name":"","type":"bytes"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"amount","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"","type":"bytes"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"amount","type":"uint256"},{"name":"gasLimit","type":"uint256"},{"name":"tokenGasPrice","type":"uint256"},{"name":"baseGasCharge","type":"uint256"},{"name":"tokenReceiver","type":"address"},{"name":"data","type":"bytes"}],"name":"approveAndExecuteWithSpecificGasAndChargeForIt","outputs":[{"name":"success","type":"bool"},{"name":"returnData","type":"bytes"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"gasLimit","type":"uint256"},{"name":"data","type":"bytes"}],"name":"executeWithSpecificGas","outputs":[{"name":"success","type":"bool"},{"name":"returnData","type":"bytes"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"amount","type":"uint256"},{"name":"gasLimit","type":"uint256"},{"name":"tokenGasPrice","type":"uint256"},{"name":"baseGasCharge","type":"uint256"},{"name":"tokenReceiver","type":"address"}],"name":"transferAndChargeForGas","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"sandAdmin","type":"address"},{"name":"executionAdmin","type":"address"},{"name":"beneficiary","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"superOperator","type":"address"},{"indexed":false,"name":"enabled","type":"bool"}],"name":"SuperOperator","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"oldAdmin","type":"address"},{"indexed":false,"name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"oldAdmin","type":"address"},{"indexed":false,"name":"newAdmin","type":"address"}],"name":"ExecutionAdminAdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"executionOperator","type":"address"},{"indexed":false,"name":"enabled","type":"bool"}],"name":"ExecutionOperator","type":"event"}]'
+    abi = tokens['time']['abi']
     abi = json.loads(abi)
     contract = web3.eth.contract(address=checksum_address, abi=abi)
-    balance_wei = contract.functions.balanceOf(wal_checksum).call()
-    balance = web3.fromWei(balance_wei, 'ether')
+    balance_gwei = contract.functions.balanceOf(wal_checksum).call()
+    balance = web3.fromWei(balance_gwei, 'gwei')
     return balance
 
 
 @app.callback(
     Output(
-        component_id='my-output',
+        component_id='time_price',
         component_property='children'
-        ),
+    ),
+    Output(
+        component_id='time_balance',
+        component_property='children'
+    ),
+    Output(
+        component_id='time_value',
+        component_property='children'
+    ),
+    Input(
+        component_id='price_interval',
+        component_property='n_intervals'
+    ),
+    Input(
+        component_id='wallet_input',
+        component_property='valid'
+    ),
     Input(
         component_id='wallet_input',
         component_property='value'
-        ),
+    ),
 )
-def update_output_div(input_value):
-    balance = get_token_balance(wal_addr=input_value)
-    return f'SAND balance: {balance}'
-
+def time_value(n, valid, value):
+    ''' If the wallet address is valid populate TIME balance, price, value
+    '''
+    if valid:
+        time_price = cg.get_price(
+            ids='wonderland',
+            vs_currencies='usd'
+        )
+        time_price_2 = time_price['wonderland']['usd'] # type float
+        time_price_3 = '$' + str(time_price_2)
+        balance = get_token_balance(wal_addr=value)
+        time_value = '$' + str(time_price_2*float(balance))
+    else:
+        time_price_3 = '$0'
+        balance = '0'
+        time_value = '$0'
+    return time_price_3, balance, time_value
 
 if __name__ == '__main__':
     app.run_server(debug=True)
